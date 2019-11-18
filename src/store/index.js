196ffb6f2cde4adabf07f02c25b5373f10e553da
git_master <h-furukawa@it-one.co.jp>
2019-11-18 11:19:17 +0900

    setup store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VuexPersistance from 'vuex-persist';
+import mutations from './mutations';
+import actions from './actions';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+const vuexLocal = new VuexPersistance({
+    storage: window.localStorage
+});
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {},
+    mutations,
+    actions,
+    getters: {},
+    plugins: [vuexLocal.plugin],
+    strict: debug
+});
